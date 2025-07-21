c6919fa8475988304ab0de3531949c2733f7174a
Pmodugula89 <modugulp@csp.edu>
2025-07-21 12:47:18 -0400

    Create data_fetch.py

---
+++ data_fetch.py
@@ -0,0 +1,9 @@
+import pandas as pd
+def fetch_data():
+    # Simulate pulling data from gaming platform
+    data = pd.DataFrame({
+        'Game': ['Poker', 'Blackjack', 'Roulette'],
+        'Players': [1200, 950, 670],
+        'Revenue': [15000, 12000, 8000]
+    })
+    return data
